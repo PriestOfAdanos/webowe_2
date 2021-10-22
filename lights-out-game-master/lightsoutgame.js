7107e31bcc7b6d6d550b5cf7be3199f3ef1f2514
phabrzyk <phabrzyk@autnomous-systems.pl>
2021-10-22 22:02:36 +0200

    now works :p

--- lights-out-game-master/lightsoutgame.js
+++ lights-out-game-master/lightsoutgame.js
@@ -1,43 +1,23 @@
-// Ensure the DOM is loaded before attempting to mutate or add elements.
 window.onload = function () {
-
-  // Use a multidemnsional array to represent the x and y respectively 
-  // where the value is a boolean to represent whether the light is on
   let boardContainer = [];
-
   let xlength;
   let ylength;
-
-
   let attemptsCounter = 0;
   let boardCompleted = false;
-
   let boardDiv = document.getElementById("board");
   let restartButton = document.getElementById("restart");
-  let newBoardButton = document.getElementById("newboard");
   let createGameForm = document.forms["createGameForm"];
   let attemptsPara = document.getElementById("attempts");
-
-  // Initally hide game option functionality 
   restartButton.style.display = 'none';
-  newBoardButton.style.display = 'none';
-
-  // Once DOM items are loaded attach the event handlers
   createGameForm.onsubmit = e => newGame(e);
   restartButton.onclick = e => restartGame(e);
-  newBoardButton.onclick = e => showGameForm(e);
-
   let StepsToSolution = [];
 
   function newGame(e) {
     e.preventDefault();
-    // Update x and y lengths
-    xlength = createGameForm["width"].value;
-    ylength = createGameForm["height"].value;
     createGame();
     createGameForm.style.display = 'none';
     restartButton.style.display = 'inline';
-    newBoardButton.style.display = 'inline';
     attemptsPara.style.display = 'block'; 
   }
 
@@ -49,7 +29,6 @@ window.onload = function () {
     createGameForm.style.display = 'inline';
     attemptsPara.style.display = 'none';
     restartButton.style.display = 'none';
-    newBoardButton.style.display = 'none';
   }
 
   function restartGame(e) {
@@ -58,27 +37,21 @@ window.onload = function () {
   }
 
   function createGame() {
-    // Update board container
     boardContainer = createBoard(xlength, ylength);
-    // Reset counter
     attemptsCounter = 0;
-    attemptsPara.innerHTML = `Attempts: ${attemptsCounter}`;
-    // Reset board completed
+    attemptsPara.innerHTML = `Ruchy: ${attemptsCounter}`;
     boardCompleted = false;
-    // Finally display the board
     displayBoard(boardContainer);
   }
 
   function userClick(x, y) {
     if (!boardCompleted) { 
-      // Update attempts counter information
       attemptsCounter++;
-      attemptsPara.innerHTML = `Attempts: ${attemptsCounter}`;
+      attemptsPara.innerHTML = `Ruchy: ${attemptsCounter}`;
       updateBoard(boardContainer, x, y);
       if (checkBoardCompleted(boardContainer)) {
         boardCompleted = true;
-        let attemptText = attemptsCounter === 1 ? 'attempt' : 'attempts';
-        attemptsPara.innerHTML = `Congratulations! Completed in ${attemptsCounter} ${attemptText}. Restart or update the board size to continue.`;
+        attemptsPara.innerHTML = `Gratulacje! Udało ci się w  ${attemptsCounter} Ruchach.`;
       }
       displayBoard(boardContainer);
     }
@@ -88,38 +61,38 @@ window.onload = function () {
     while (boardDiv.hasChildNodes()) {
       boardDiv.removeChild(boardDiv.lastChild);
     }
-    for (i=0; i<ylength; i++) {
+    for (i=0; i<7; i++) {
       let row = document.createElement('div');
       row.className = 'row';
-      for (j=0; j<xlength; j++) {
+      for (j=0; j<7; j++) {
         let boardItem = document.createElement('div');
-        console.log(board);
-        if(board[i][j]===0){boardItem.className='boardItemRed'; console.log("aaa");}
-        else if(board[i][j]===1){boardItem.className='boardItemBlue'; console.log("bbb");}
-        else if(board[i][j]===2){boardItem.className='boardItemGreen'; console.log("ccc");}        
+        if(Number(board[i][j])===0){boardItem.className='boardItemRed';}
+        if(Number(board[i][j])===1){boardItem.className='boardItemBlue';}
+        if(Number(board[i][j])===2){boardItem.className='boardItemGreen';}        
         boardItem.onclick = userClick.bind(undefined, i, j);
         row.appendChild(boardItem);
       }
       boardDiv.appendChild(row);
     }
+    console.log(StepsToSolution);
   }
 
   function createBoard(x, y) {
-    let newBoard =  new Array(x);
+    let newBoard =  new Array(7);
 
-    for (var i = 0; i < x; i++) {
-      newBoard[i] = new Array(x);
+    for (var i = 0; i < 7; i++) {
+      newBoard[i] = new Array(7).fill(0);
     }
-    for(k=0;k<y;k++){//randomly make as many moves as 1 dimension of board
-      addRandomMovesToMap(y)
+    for (var i = 0; i < 100; i++) {
+      addRandomMovesToMap(newBoard)
     }
     return newBoard;
-
   }
 
-  function addRandomMovesToMap(edge_length){
-    x = Math.floor(Math.random() * edge_length);
-    y = Math.floor(Math.random() * edge_length);
+  function addRandomMovesToMap(board){
+    x = Math.floor(Math.random() * 7);
+    y = Math.floor(Math.random() * 7);
+    updateBoard(board,x,y)
     addMoveToList(x,y);
   }
 
@@ -129,14 +102,14 @@ window.onload = function () {
   }
 
   function updateBoard(board, x, y) {
-    board[x][y] = !board[x][y];
+    board[x][y] = (board[x][y]+=1)%3;
     if (x > 0)                  { board[x-1][y] = (board[x-1][y]+=1)%3; }
     if (x < board.length-1)     { board[x+1][y] = (board[x+1][y]+=1)%3; }
     if (y > 0)                  { board[x][y-1] = (board[x][y-1]+=1)%3; }
     if (y < board[0].length-1)  { board[x][y+1] = (board[x][y+1]+=1)%3; }
 
     if(StepsToSolution.at(-1)===(x,y)){
-      anArray.pop();
+      StepsToSolution.pop();
     }else if(StepsToSolution.at(-1)!=(x,y)){
       addMoveToList(x,y);
     }
